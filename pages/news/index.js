b569e9faa2b3302826f4c9a77e248cc2f945b516
kbomb2003 <kbomb2003@gmail.com>
2023-05-11 20:02:21 +0700

    update

---
+++ pages/news/index.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const index = () => {
+  const uri = 'http://localhost:3000/api/news';
+  const [data, setData] = useState(null);
+  //javascript
+  useEffect(() => {
+    //code
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    //code
+    await axios
+      .get(uri)
+      .then((res) => {
+        console.log(res.data.listUsers);
+        setData(res.data.listUsers);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <hr />
+      <ul>
+        {data &&
+          data.map((item, index) => <li key={index}>{item.first_name}</li>)}
+      </ul>
+    </>
+  );
+};
+
+export default index;
